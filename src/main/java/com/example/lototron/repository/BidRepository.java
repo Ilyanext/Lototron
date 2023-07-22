56b628bd1f032256f5351a6eca215cd54616e609
Ilya <rabota003@yandex.ru>
2023-07-22 18:06:35 +0300

    data 22/07/23 question BidRepository and LotRepository

--- src/main/java/com/example/lototron/repository/BidRepository.java
+++ src/main/java/com/example/lototron/repository/BidRepository.java
@@ -2,12 +2,14 @@ package com.example.lototron.repository;
 
 import com.example.lototron.model.BidModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BidRepository extends JpaRepository<BidModel, Integer> {
+
     @Query(value = "select *from bid where lot_id = :id order by bidder_date_time limit 1", nativeQuery = true)
     BidModel getInfoFirstBidder(@Param("id") int id);
 
